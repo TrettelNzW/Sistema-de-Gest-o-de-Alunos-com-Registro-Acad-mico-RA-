282f4c2fe644ad627bbae9445a69eefb0ce58935
Arthur Silveira Trettel <127143387+TrettelNzW@users.noreply.github.com>
2024-05-01 13:44:40 -0300

    Create Aluno.java

---
+++ Aluno.java
@@ -0,0 +1,17 @@
+public class Aluno {
+    String nome;
+    int idade;
+
+    Aluno(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    String Nome() {
+        return nome;
+    }
+
+    int Idade() {
+        return idade;
+    }
+}
